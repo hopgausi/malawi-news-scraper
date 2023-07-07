9245fbae6b2291f171b9de4915076baeb156031b
Hopson Gausi <hopgausi20@gmail.com>
2023-07-08 01:23:56 +0200

    Added the national malawi news outlet

--- src/scrapers/__init__.py
+++ src/scrapers/__init__.py
@@ -1,10 +1,14 @@
 from importlib import resources
 import tomllib
+from .malawi_voice import MalawiVoiceParser
+from .maravi_post import MaraviPostParser
+from .mwnation import MwNationParser
+
 
 # Version of the malawi news scraper
 __version__ = "1.1.1"
 
-
 _cfg = tomllib.loads(resources.read_text("scrapers", "config.toml"))
 MALAWI_VOICE_URL = _cfg["scraper_urls"]["malawi_voice"]
 MARAVI_POST_URL = _cfg["scraper_urls"]["maravi_post"]
+MWNATION_URL = _cfg["scraper_urls"]["mwnation"]
