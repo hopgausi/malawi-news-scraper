44b4a52d90425bd23b50928d818a2d9e852a679c
Hopson Gausi <hopgausi20@gmail.com>
2023-07-07 19:26:43 +0200

    bump version 1.0.0 -> 1.1.0

--- src/scrapers/__init__.py
+++ src/scrapers/__init__.py
@@ -2,7 +2,7 @@ from importlib import resources
 import tomllib
 
 # Version of the malawi news scraper
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 
 _cfg = tomllib.loads(resources.read_text("scrapers", "config.toml"))
